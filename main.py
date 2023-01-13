8438cc59d428da6226d10e050f9f4b78ba9f8ef2
kevin pinsage <kpinsag@gmail.com>
2023-01-13 12:41:22 -0500

    cambios

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from flask import Flask
 app= Flask(__name__)
 
 
-@app.route('/')
+@app.route('/kevinpinsag')
 def Hello1():
     return 'Program 1 with flask by kevin pinsag'
+
+@app.route('/')
+def Hello2():
+    return 'primero desarrollo de software yavirac'
+
+#if__name__=="__main__":
+    app.run(port=8080)
+    
+if __name__== '__main__':
+    app.run(debug=True, port=8080)
